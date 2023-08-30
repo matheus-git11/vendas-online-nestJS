cdeedaab422db825a52a191697bbe72f24eb4689
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-08-30 18:17:04 -0300

    criando typeOrm com postgress

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,19 @@
 import { Body, Controller, Post, Get } from '@nestjs/common';
 import { createUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
-import { User } from './interface/user.interface';
+import { UserEntity } from './interface/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async createUser(@Body() createUser: createUserDto): Promise<User> {
+  async createUser(@Body() createUser: createUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser);
   }
 
   @Get()
-  async getAllUsers(): Promise<User[]> {
+  async getAllUsers(): Promise<UserEntity[]> {
     return this.userService.getAllUsers();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { createUserDto } from './dtos/createUser.dto';
-import { User } from './interface/user.interface';
+import { UserEntity } from './interface/user.entity';
 import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-  async createUser(createUserDto: createUserDto): Promise<User> {
+  async createUser(createUserDto: createUserDto): Promise<UserEntity> {
     const salt = 10;
     const hashedPassword = await hash(createUserDto.password, salt);
-    const user: User = {
+    return this.userRepository.save({
       ...createUserDto,
-      id: this.users.length + 1,
       password: hashedPassword,
-    };
-    this.users.push(user);
-    return user;
+    });
   }
 
-  async getAllUsers(): Promise<User[]> {
-    return this.users;
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 }
