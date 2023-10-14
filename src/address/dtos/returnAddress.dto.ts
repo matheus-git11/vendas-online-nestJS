e985dd9db5faeba3092e7570d5401eb55750996b
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-10-14 14:04:25 -0300

    adicionando Dtos de endereco,estado e cidade

--- src/address/dtos/returnAddress.dto.ts
+++ src/address/dtos/returnAddress.dto.ts
@@ -1,13 +1,16 @@
+import { returnCityDto } from 'src/city/dtos/returnCity.dto';
 import { AdresssEntity } from '../entities/address.entity';
 
 export class ReturnAddressDto {
   complement: string;
   numberAddress: number;
   cep: string;
+  city?: returnCityDto;
 
   constructor(address: AdresssEntity) {
     this.complement = address.complement;
     this.numberAddress = address.numberAddress;
     this.cep = address.cep;
+    this.city = address.city ? new returnCityDto(address.city) : undefined;
   }
 }
